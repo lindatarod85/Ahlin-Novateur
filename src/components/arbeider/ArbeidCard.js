7d00baf54f578cc199952a42e762347926b74a37
lindatarod85 <lindatarod@gmail.com>
2024-01-24 15:06:03 +0100

    opt

--- src/components/arbeider/ArbeidCard.js
+++ src/components/arbeider/ArbeidCard.js
@@ -1,16 +1,30 @@
 import styles from '../../styles/ArbeidCard.module.css';
+import { useEffect } from 'react';
 
 function ArbeidCard({ arbeider }) {
+  useEffect(() => {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'preload';
+    linkElement.href = arbeider.featured_media_src_url;
+    linkElement.as = 'image';
+    document.head.appendChild(linkElement);
 
-    return (
-      <div className={styles.arbeidCard}>
-        <img src={arbeider.featured_media_src_url} alt="arbeid"  />
-        <h2>{arbeider.title && arbeider.title.rendered}</h2>
-      </div>
-    );
-  }
-  
-  export default ArbeidCard;
+    return () => {
+      // Cleanup if necessary
+      document.head.removeChild(linkElement);
+    };
+  }, [arbeider.featured_media_src_url]);
+
+  return (
+    <div className={styles.arbeidCard}>
+      <img src={arbeider.featured_media_src_url} alt="arbeid" loading="lazy" />
+      <h2>{arbeider.title && arbeider.title.rendered}</h2>
+    </div>
+  );
+}
+
+export default ArbeidCard;
+
   
 
 //<img src={arbeid.featured_image} alt={arbeid.title.rendered} />
